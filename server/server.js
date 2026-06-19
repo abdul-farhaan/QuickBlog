cfc4ffe3b3e78c890b341bec3ea8f1f78b8f15b9
abdul-farhaan <abdulfarhaan0327@gmail.com>
2026-06-19 18:34:23 +0530

    init: backend server

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import 'dotenv/config'
+import cors from 'cors'
+
+const app = express(); // object to which all the routes and middleware are added to this app
+
+// Middleware
+app.use(cors())
+app.use(express.json()) //parse data
+
+app.get('/', (req,res)=> res.send("API is Working")) //whenoutputs "API is Working"
+
+const PORT = process.env.PORT || 3000; //take post from .env if defined else take 3000
+
+app.listen(PORT, ()=>{
+    console.log('Server is running on port '+ PORT) //terminal output
+}) 
+
+export default app;
